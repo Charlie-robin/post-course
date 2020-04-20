f1be8097748423372de018bb6b22ea0dec65644e
Charlie Richardson <Charlie.Richardson@Nology.local>
2020-04-20 13:25:10 +0100

    katas 10

---
+++ Javascript Katas/completed/kata-10.js
@@ -0,0 +1,24 @@
+// How do you print the first non-repeated character from a string?
+
+const stringToArray = (string) => string.toLowerCase().split("");
+
+const printFirstNonRepeatedCharacter = (string) => {
+    const stringArray = stringToArray(string);
+    const nonRepeatedArray = stringArray.filter((element => stringArray.indexOf(element) === stringArray.lastIndexOf(element)));
+    return nonRepeatedArray[0];
+
+}
+
+// const printFirstNonRepeatedCharacter = (string) => {
+//   const stringArray = stringToArray(string);
+//   const filterNonRepeatedChar = stringArray.filter((element, index) => element !== stringArray[index-1] && element !== stringArray[index+1]);
+//   return filterNonRepeatedChar[0];
+// };
+
+
+
+
+console.log(printFirstNonRepeatedCharacter("hheello"), "o")
+console.log(printFirstNonRepeatedCharacter("disco"), "d")
+console.log(printFirstNonRepeatedCharacter("functionfnction"), "u")
+console.log(printFirstNonRepeatedCharacter("helo"), "h")
