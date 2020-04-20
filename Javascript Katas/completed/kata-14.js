f09cea018bfe7274575a0bc0a8fe1ffda2cd4ba8
Charlie Richardson <Charlie.Richardson@Nology.local>
2020-04-20 20:22:29 +0100

    kata 14

---
+++ Javascript Katas/completed/kata-14.js
@@ -0,0 +1,8 @@
+// How do you swap two numbers without using a third variable?
+
+const swapNumbers = (numberArray) => numberArray.reverse();
+
+console.log(swapNumbers([10,15]), "15,10")
+console.log(swapNumbers([100,1995]),"1995, 100" )
+console.log(swapNumbers([-10,-15]), "-15, -10")
+
