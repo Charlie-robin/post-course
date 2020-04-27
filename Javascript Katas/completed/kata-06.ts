67cb9b79e47f63fec303a007379b7fff3e0dcc60
Charlie Richardson <Charlie.Richardson@Nology.local>
2020-04-27 21:21:34 +0100

    Line by line die section on kata 6 as my solution didnt work as well as his

---
+++ Javascript Katas/completed/kata-06.ts
@@ -0,0 +1,66 @@
+// How do you find all pairs of an integer array whose sum is equal to a given number?
+
+const findPairsWhichSum = (numberArray, matchNum) => {
+  const result = [... new Set(numberArray)]
+         .filter((currentNumber) => {
+           if (matchNum / 2 > currentNumber) {
+             return false;
+           }
+           if (matchNum / 2 === currentNumber) {
+             return (numberArray.filter((element) => currentNumber === element).length > 1);
+           }
+           return ((matchNum - currentNumber) in numberArray);
+         })
+         .map((element) => [element, matchNum - element]);
+
+         return result;
+ }
+
+// const findPairsWhichSum = (array, matchNumber) => {
+//   const pairMatch = [];
+//   const arrayCopy = [...array];
+//   for (let index = 0; index < array.length; index++) {
+//     const numToMatch = matchNumber - arrayCopy[index];
+//     if (arrayCopy.indexOf(numToMatch) !== -1) {
+//       pairMatch.unshift(arrayCopy[index]);
+//     }
+//   }
+//   const filterPair = pairMatch.filter((element, index) => pairMatch.indexOf(element) === index);
+//   return filterPair.map(element => [element, matchNumber - element]);
+  
+// }
+
+
+// console.log(findPairsWhichSum([1, 2, 2, 3, 3, 5, 6, 7, 7], 4));
+
+// console.log(findPairsWhichSum([5, 2, 2, 8, 4, 5, 6, 6, 7, 7], 12));
+// // console.log(findPairsWhichSum([5, 2, 2, 4, 8, 5, 6, 7, 7], 12));
+
+
+
+// console.log Javascript Katas/solutions/q06-solution.ts:17
+// [ [ 2, 2 ], [ 3, 1 ] ]
+
+// console.log Javascript Katas/solutions/q06-solution.ts:17
+// [ [ 8, 4 ], [ 7, 5 ] ]
+
+// console.log Javascript Katas/solutions/q06-solution.ts:17
+// [ [ 8, 4 ], [ 6, 6 ], [ 7, 5 ] ]
+
+// export const findPairsWhichSum = (array, matchNumber) => {
+//   const result = array.filter(
+//     (element, index) =>
+//       element + array[index - 1] === matchNumber || element + array[index + 1] === matchNumber
+//   );
+//   console.log(result);
+//   return result;
+// };
+
+//  export const findPairsWhichSum = (array, matchNumber) => {
+//   const match = [];
+//   for (let index = 0; index < array.length; index++) {
+//     array.forEach((element, arrIndex) => element + array[arrIndex] === matchNumber ? match.push(element): null);
+//   }
+//   console.log(match);
+//   return match;
+// };
